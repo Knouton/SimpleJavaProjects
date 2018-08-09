b734c872d809c87f20cebf05334f0021dd3aa8e3
Knouton <knoutmefis@gmail.com>
2018-08-09 06:18:21 +0500

    Create JavaEchoServer.java

---
+++ JavaEchoServer.java
@@ -0,0 +1,47 @@
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
+public class dadad {
+        public static void main(String[] args)
+        {
+            try
+            {
+                // Устанавливаем сервер сокет
+                ServerSocket s = new ServerSocket(8080);
+                while(true) {
+                    System.out.println("Wait for TCP-connection");
+                    // Ждём подключения
+                    Socket incoming = s.accept();
+                    System.out.println("Get some connection");
+                    try(InputStream inStream = incoming.getInputStream();
+                        OutputStream outStream = incoming.getOutputStream();
+                    )
+                    {
+                        Scanner in = new Scanner(inStream);
+                        PrintWriter out = new PrintWriter(outStream, true /* autoFlush */);
+
+                        out.println("Hello! Enter BYE to exit.");
+
+                        // Эхо ответ
+                        boolean done = false;
+                        while (!done && in.hasNextLine())
+                        {
+                            String line = in.nextLine();
+                            out.println("Echo: " + line);
+                            if (line.trim().equals("BYE")) done = true;
+                        }
+                    }
+                    finally
+                    {
+                        incoming.close();
+                    }
+                }
+
+            }
+            catch (IOException e)
+            {
+                e.printStackTrace();
+            }
+    }
+}
