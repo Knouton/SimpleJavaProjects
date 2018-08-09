bb28394aa95dc691191098a0018758d51af35767
Knouton <knoutmefis@gmail.com>
2018-08-09 06:19:42 +0500

    Update and rename JavaEchoServer.java to JavaHTTPEchoServer.java

--- JavaHTTPEchoServer.java
+++ JavaHTTPEchoServer.java
@@ -2,7 +2,7 @@ import java.io.*;
 import java.net.*;
 import java.util.*;
 
-public class dadad {
+public class JavaHTTPEchoServer {
         public static void main(String[] args)
         {
             try
