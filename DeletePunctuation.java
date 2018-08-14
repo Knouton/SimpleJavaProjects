9aa59a5da776dedd97137b149f4bb409ee2ee0a0
Knouton <knoutmefis@gmail.com>
2018-08-14 05:52:42 +0500

    Update DeletePunctuation.java

--- DeletePunctuation.java
+++ DeletePunctuation.java
@@ -7,25 +7,31 @@ import java.util.regex.Pattern;
 //Считать содержимое первого файла, удалить все знаки пунктуации, включая символы новой строки.
 public class Solution {
     public static void main(String[] args) throws IOException {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        BufferedReader readerFile = new BufferedReader(new FileReader(reader.readLine()));
-        BufferedWriter writerFile = new BufferedWriter(new FileWriter(reader.readLine()));
-        int numRead;
-        char buf[] = new char[1024];
-        StringBuilder stringBuilder = new StringBuilder();
-        while (readerFile.ready()) {
-            numRead  = readerFile.read(buf);
-            String readData = String.valueOf(buf, 0, numRead);
-            stringBuilder.append(readData);
-        }
-        Pattern pattern = Pattern.compile("\\p{Punct}");
-        Matcher matcher = pattern.matcher(stringBuilder);
-        String space = "";
-        String s = String.valueOf(stringBuilder);
-        s = matcher.replaceAll(space);
-        writerFile.write(s);
-        writerFile.close();
-        reader.close();
-        readerFile.close();
+        BufferedReader reader = null;
+            BufferedReader readerFile = null;
+            BufferedWriter writerFile = null;
+            try{
+                reader = new BufferedReader(new InputStreamReader(System.in));
+                readerFile = new BufferedReader(new FileReader(reader.readLine()));
+                writerFile = new BufferedWriter(new FileWriter(reader.readLine()));
+                int numRead;
+                char buf[] = new char[1024];
+                StringBuilder stringBuilder = new StringBuilder();
+                while (readerFile.ready()) {
+                    numRead  = readerFile.read(buf);
+                    String readData = String.valueOf(buf, 0, numRead);
+                    stringBuilder.append(readData);
+                }
+                Pattern pattern = Pattern.compile("\\p{Punct}");
+                Matcher matcher = pattern.matcher(stringBuilder);
+                String space = "";
+                String s = String.valueOf(stringBuilder);
+                s = matcher.replaceAll(space);
+                writerFile.write(s);
+            } finally {
+                writerFile.close();
+                reader.close();
+                readerFile.close();
+            }
     }
 }
