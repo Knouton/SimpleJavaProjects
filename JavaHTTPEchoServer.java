50cd35730f8135c459784892df5bdc9853c17e7f
Knouton <knoutmefis@gmail.com>
2018-08-14 05:56:15 +0500

    Update JavaHTTPEchoServer.java

--- JavaHTTPEchoServer.java
+++ JavaHTTPEchoServer.java
@@ -3,45 +3,45 @@ import java.net.*;
 import java.util.*;
 
 public class JavaHTTPEchoServer {
-        public static void main(String[] args)
+        public static void main(String[] args) {
+        try
         {
-            try
-            {
-                // Устанавливаем сервер сокет
-                ServerSocket s = new ServerSocket(8080);
-                while(true) {
-                    System.out.println("Wait for TCP-connection");
-                    // Ждём подключения
-                    Socket incoming = s.accept();
-                    System.out.println("Get some connection");
-                    try(InputStream inStream = incoming.getInputStream();
-                        OutputStream outStream = incoming.getOutputStream();
-                    )
-                    {
-                        Scanner in = new Scanner(inStream);
-                        PrintWriter out = new PrintWriter(outStream, true /* autoFlush */);
+            // Устанавливаем сервер сокет
+            ServerSocket socket = new ServerSocket(8080);
+            while(true) {
+                System.out.println("Wait for TCP-connection");
+                // Ждём подключения
+                Socket incoming = socket.accept();
+                System.out.println("Get some connection");
+                try(InputStream inStream = incoming.getInputStream();
+                    OutputStream outStream = incoming.getOutputStream();
+                )
+                {
+                    Scanner in = new Scanner(inStream);
+                    PrintWriter out = new PrintWriter(outStream, true /* autoFlush */);
 
-                        out.println("Hello! Enter BYE to exit.");
+                    out.println("Hello! Enter BYE to exit.");
 
-                        // Эхо ответ
-                        boolean done = false;
-                        while (!done && in.hasNextLine())
-                        {
-                            String line = in.nextLine();
-                            out.println("Echo: " + line);
-                            if (line.trim().equals("BYE")) done = true;
-                        }
-                    }
-                    finally
+                    // Эхо ответ
+                    boolean done = false;
+                    while (!done && in.hasNextLine())
                     {
-                        incoming.close();
+                        String line = in.nextLine();
+                        out.println("Echo: " + line);
+                        if (line.trim().equals("BYE")) done = true;
                     }
                 }
+                finally
+                {
+                    incoming.close();
+                    socket.close();
+                }
+            }
 
-            }
-            catch (IOException e)
-            {
-                e.printStackTrace();
-            }
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
     }
 }
